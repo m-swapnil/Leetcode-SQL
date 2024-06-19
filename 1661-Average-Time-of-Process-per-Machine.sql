485d8d6fbb7b7ceb66148627020a5bba1fa4471b
Swapnil Mishra <155835526+datawizswap@users.noreply.github.com>
2024-06-19 20:33:24 +0530

    Create 1661-Average-Time-of-Process-per-Machine.sql

---
+++ 1661-Average-Time-of-Process-per-Machine.sql
@@ -0,0 +1,13 @@
+SELECT
+    StartActivity.machine_id,
+    ROUND(AVG(EndActivity.timestamp - StartActivity.timestamp), 3) AS processing_time
+FROM
+    Activity AS StartActivity
+    JOIN Activity AS EndActivity
+    ON StartActivity.machine_id = EndActivity.machine_id
+    AND StartActivity.process_id = EndActivity.process_id
+WHERE
+    StartActivity.activity_type = 'start'
+    AND EndActivity.activity_type = 'end'
+GROUP BY
+    StartActivity.machine_id;
