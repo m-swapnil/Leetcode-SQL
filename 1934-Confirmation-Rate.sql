fd0462f51434916ef9bb049e7da0283d38854ba6
Swapnil Mishra <155835526+datawizswap@users.noreply.github.com>
2024-06-23 01:55:21 +0530

    Create 1934-Confirmation-Rate.sql

---
+++ 1934-Confirmation-Rate.sql
@@ -0,0 +1,11 @@
+select
+    signups.user_id,
+    round(avg(if(action='confirmed',1,0)),2) as confirmation_rate
+from
+    signups
+left join
+    confirmations
+    on
+    signups.user_id = confirmations.user_id
+group by
+    user_id;
