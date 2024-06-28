09396c402cca6468c20c29d1d17ee0ab1ca18307
Swapnil Mishra <155835526+datawizswap@users.noreply.github.com>
2024-06-28 12:32:06 +0530

    Create 1211-Queries-Quality-and-Percentage.sql

---
+++ 1211-Queries-Quality-and-Percentage.sql
@@ -0,0 +1,13 @@
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(
+        (COUNT(CASE WHEN rating < 3 THEN 1 END) * 100.0 / COUNT(*)), 
+        2
+    ) AS poor_query_percentage
+FROM 
+    Queries
+GROUP BY
+    query_name
+HAVING
+    query_name IS NOT NULL;
