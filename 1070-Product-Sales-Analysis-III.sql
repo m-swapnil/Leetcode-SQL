127c0415a7d0ae516f7d001ac4f0927d2d6a3f90
Swapnil Mishra <155835526+datawizswap@users.noreply.github.com>
2024-07-04 21:37:43 +0530

    Create 1070-Product-Sales-Analysis-III.sql

---
+++ 1070-Product-Sales-Analysis-III.sql
@@ -0,0 +1,9 @@
+WITH FirstYearSales AS (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)
+SELECT s.product_id, f.first_year, s.quantity, s.price
+FROM FirstYearSales f
+JOIN Sales s
+ON f.product_id = s.product_id AND f.first_year = s.year;
