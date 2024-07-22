ec468d5e4dca85027d1a36aa54bda55571e5b66d
Swapnil Mishra <155835526+datawizswap@users.noreply.github.com>
2024-07-22 10:31:00 +0530

    Create 185-Department-Top-Three-Salaries.sql

---
+++ 185-Department-Top-Three-Salaries.sql
@@ -0,0 +1,11 @@
+SELECT
+    D.name AS Department,
+    E.name AS Employee,
+    E.salary AS Salary
+FROM Department AS D
+INNER JOIN Employee AS E ON D.id = E.departmentId
+WHERE (
+    SELECT COUNT(DISTINCT E2.salary)
+    FROM Employee AS E2
+    WHERE E2.departmentId = E.departmentId AND E2.salary > E.salary
+) < 3;
