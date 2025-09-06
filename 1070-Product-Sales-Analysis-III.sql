d034d8291594206f94ac24c0c9eced581fdab8d8
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-09-07 00:44:11 +0530

    Update 1070-Product-Sales-Analysis-III.sql

--- 1070-Product-Sales-Analysis-III.sql
+++ 1070-Product-Sales-Analysis-III.sql
@@ -2,6 +2,7 @@ WITH FirstYearSales AS (
     SELECT product_id, MIN(year) AS first_year
     FROM Sales
     GROUP BY product_id
+    
 )
 SELECT s.product_id, f.first_year, s.quantity, s.price
 FROM FirstYearSales f
