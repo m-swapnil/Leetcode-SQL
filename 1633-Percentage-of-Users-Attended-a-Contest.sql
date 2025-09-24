87ea4b817040bf7f891052a479c5dc50e924d441
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-09-24 18:28:28 +0530

    Update 1633-Percentage-of-Users-Attended-a-Contest.sql

--- 1633-Percentage-of-Users-Attended-a-Contest.sql
+++ 1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -3,7 +3,7 @@ SELECT
     ROUND(COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
 FROM 
     Register
-GROUP BY 
+GROUP BY
     contest_id
 ORDER BY 
     percentage DESC, contest_id ASC;
