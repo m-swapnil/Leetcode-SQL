782e4ab8ac790c7a87169d907942e8f04455eb1e
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-10-01 01:26:28 +0530

    Update 1321-Restaurant-Growth.sql

--- 1321-Restaurant-Growth.sql
+++ 1321-Restaurant-Growth.sql
@@ -4,5 +4,5 @@ select a.visited_on,
 from (select visited_on, sum(amount) amount from Customer GROUP BY visited_on) a, 
     (select visited_on, sum(amount) amount from Customer GROUP BY visited_on) b 
 where datediff(a.visited_on,b.visited_on) between 0 and 6
-group by a.visited_on 
+group by a.visited_on  
 having count(distinct b.visited_on) = 7;
