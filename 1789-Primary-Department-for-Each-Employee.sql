e16cde9df34d39305c888ec1300b52e4f61d21dd
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-10-11 17:18:49 +0530

    Update 1789-Primary-Department-for-Each-Employee.sql

--- 1789-Primary-Department-for-Each-Employee.sql
+++ 1789-Primary-Department-for-Each-Employee.sql
@@ -2,7 +2,7 @@ select employee_id, department_id
 from employee
 group by employee_id
 having count(employee_id)=1
-union
+union 
 select employee_id, department_id
 from employee
 where primary_flag = 'Y';
