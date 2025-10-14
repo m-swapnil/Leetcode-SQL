25dc855cba735b30e931a29489efd069d798e0c4
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-10-14 17:53:35 +0530

    Update 1907-Count-Salary-Categories.sql

--- 1907-Count-Salary-Categories.sql
+++ 1907-Count-Salary-Categories.sql
@@ -3,7 +3,7 @@ FROM Accounts
 WHERE income < 20000
 UNION ALL
 SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count
-FROM Accounts
+FROM Accounts 
 WHERE income BETWEEN 20000 AND 50000 
 UNION ALL
 SELECT 'High Salary' AS category, COUNT(*) AS accounts_count
