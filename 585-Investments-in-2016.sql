76964cc653b9a828f772bc348add95fff71d88d4
Swapnil Mishra <155835526+m-swapnil@users.noreply.github.com>
2025-10-30 21:45:17 +0530

    Fix SQL syntax for city_count calculation

--- 585-Investments-in-2016.sql
+++ 585-Investments-in-2016.sql
@@ -2,7 +2,7 @@ WITH InsuranceWithCounts AS (
     SELECT
         tiv_2016,
         COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,
-        COUNT(*) OVER (PARTITION BY lat, lon) AS city_count
+        COUNT(*) OVER (PARTITION BY lat, lon) AS city_count 
     FROM Insurance 
 )
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016 
